17c3b1de68c9317fe0d0078f6186298ba3224aa8
KavyaPotturi <potturilakshmikavya@gmail.com>
2025-05-05 10:29:44 +0530

    updated

---
+++ Angularjs/Controller.js
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html ng-app="myApp">
+  <body ng-controller="myCtrl">
+    <h2>{{greeting}}</h2>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular.min.js"></script>
+    <script>
+      var app = angular.module('myApp', []);
+      app.controller('myCtrl', function($scope) {
+        $scope.greeting = "Welcome to Angular JS!";
+      });
+    </script>
+  </body>
+</html>
